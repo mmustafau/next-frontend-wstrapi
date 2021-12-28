5045d440122401c8186d08d2725518aedf5649b0
mustafa.unlu <mustafa.unlu@peerlog.com.tr>
2021-12-28 11:46:25 +0300

    initial

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    loader: "default",
+    domains: ["strapi-mmuplays.herokuapp.com"],
+  },
+}
+
+module.exports = nextConfig
